e1ea3f46b9543a5131c1c6bfca3e71480770654d
Yusuf Erdogan <yusuf@vvoosh.com>
2019-03-14 16:48:12 +0000

    Refine

--- bin/book.sh
+++ bin/book.sh
@@ -1,6 +1,3 @@
 echo "run scheduled job!"
-echo "call google:"
-curl https://www.google.com
-echo "call gymbox:"
-curl -i https://gymbox-ec.herokuapp.com/api/run?email=erdoganyusuf@gmail.com&password=utn8HhUgMLDY6fF
+node cli.js -c run
 echo "done"
